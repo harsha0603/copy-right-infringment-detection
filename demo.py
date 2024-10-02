e84d286497016c8732eed074a2b7c60b27db98c5
harsha0603 <harshamassss@gmail.com>
2024-10-02 23:06:23 +0530

    Logging and exception added

---
+++ demo.py
@@ -0,0 +1,10 @@
+# from copyright_infringement.logger import logging
+# from copyright_infringement.exception import CopyRight
+# import sys
+# logging.info("Testing logging code successfull")
+
+
+# try:
+#     a = 7/0
+# except Exception as e:
+#     raise CopyRight(e,sys)
